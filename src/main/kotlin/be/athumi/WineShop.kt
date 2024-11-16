e1ef91ea562681a9c7fffce8e7a155031565e4b1
ppeetermans <pieterjan.peetermans@altran.com>
2024-11-16 15:02:50 +0100

    remove code smells

--- src/main/kotlin/be/athumi/WineShop.kt
+++ src/main/kotlin/be/athumi/WineShop.kt
@@ -3,59 +3,51 @@ package be.athumi
 class WineShop(var items: List<Wine>) {
     fun next() {
         // Wine Shop logic
-        for (i in items.indices) {
-            if (items[i].name != "Bourdeaux Conservato" && items[i].name != "Bourgogne Conservato" && !items[i].name.startsWith("Event")) {
-                if (items[i].price > 0) {
-                    if (items[i].name != "Wine brewed by Alexander the Great") {
-                        items[i].price = items[i].price - 1
-                    }
+        items.forEach { item ->
+            if (item.name != "Bourdeaux Conservato" && item.name != "Bourgogne Conservato" && !item.name.startsWith("Event")) {
+                if (item.price > 0 && item.name != "Wine brewed by Alexander the Great") {
+                    item.price = item.price - 1
                 }
             } else {
-                if (items[i].price < 100) {
-                    items[i].price = items[i].price + 1
+                if (item.price < 100) {
+                    item.price = item.price + 1
 
-                    if (items[i].name.startsWith("Event")) {
-                        if (items[i].expiresInYears < 8) {
-                            if (items[i].price < 100) {
-                                items[i].price = items[i].price + 1
-                            }
+                    if (item.name.startsWith("Event")) {
+                        if (item.expiresInYears < 8 && item.price < 100) {
+                            item.price = item.price + 1
                         }
 
-                        if (items[i].expiresInYears < 3) {
-                            if (items[i].price < 100) {
-                                items[i].price = items[i].price + 2
-                            }
+                        if (item.expiresInYears < 3 && item.price < 100) {
+                            item.price = item.price + 2
                         }
                     }
                 }
             }
 
-            if (items[i].name != "Wine brewed by Alexander the Great") {
-                items[i].expiresInYears = items[i].expiresInYears - 1
-            } else if (items[i].price < 0) {
-                items[i].price = 0
+            if (item.name != "Wine brewed by Alexander the Great") {
+                item.expiresInYears = item.expiresInYears - 1
+            } else if (item.price < 0) {
+                item.price = 0
             }
 
-            if (items[i].expiresInYears < 0) {
-                if (!items[i].name.contains("Conservato")) {
-                    if (!items[i].name.contains("Event")) {
-                        if (items[i].price > 0) {
-                            if (items[i].name != "Wine brewed by Alexander the Great") {
-                                items[i].price = items[i].price - 1
-                            }
+            if (item.expiresInYears < 0) {
+                if (!item.name.contains("Conservato")) {
+                    if (!item.name.contains("Event")) {
+                        if (item.price > 0 && item.name != "Wine brewed by Alexander the Great") {
+                            item.price = item.price - 1
                         }
                     } else {
-                        items[i].price = items[i].price - items[i].price
+                        item.price = 0
                     }
                 } else {
-                    if (items[i].price < 100) {
-                        items[i].price = items[i].price + 1
+                    if (item.price < 100) {
+                        item.price = item.price + 1
                     }
                 }
             }
 
-            if (items[i].price < 0) {
-                items[i].price = 0
+            if (item.price < 0) {
+                item.price = 0
             }
         }
     }
