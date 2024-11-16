8e971c0e78678bc56d34853a2f5761ddb86ae366
ppeetermans <pieterjan.peetermans@altran.com>
2024-11-16 15:37:39 +0100

    Move wine specific logic to the Wine class

--- src/main/kotlin/be/athumi/Wine.kt
+++ src/main/kotlin/be/athumi/Wine.kt
@@ -1,3 +1,54 @@
 package be.athumi
 
-data class Wine(var name: String, var price: Int, var expiresInYears: Int)
+data class Wine(var name: String, var price: Int, var expiresInYears: Int) {
+
+    fun isConservato(): Boolean =
+            name == "Bourdeaux Conservato" || name == "Bourgogne Conservato"
+
+    fun isEventItem(): Boolean =
+            name.startsWith("Event")
+
+    fun isLegendaryItem(): Boolean =
+            name == "Wine brewed by Alexander the Great"
+    fun update() {
+        if (!isConservato() && !isEventItem()) {
+            if (price > 0 && !isLegendaryItem()) {
+                price -= 1
+            }
+        } else {
+            if (price < 100) {
+                price += 1
+                if (isEventItem()) {
+                    if (expiresInYears < 8 && price < 100) {
+                        price = price + 1
+                    }
+
+                    if (expiresInYears < 3 && price < 100) {
+                        price = price + 2
+                    }
+                }
+            }
+        }
+
+        if (!isLegendaryItem()) {
+            expiresInYears -= 1
+        }
+
+        if (expiresInYears < 0) {
+            if (!isConservato()) {
+                if (!isEventItem() && price > 0 && !isLegendaryItem()) {
+                    price -= 1
+                } else if (isEventItem()) {
+                    price = 0
+                }
+            } else if (price < 100) {
+                price += 1
+            }
+        }
+
+        if (price < 0) {
+            price = 0
+        }
+    }
+
+}
--- src/main/kotlin/be/athumi/WineShop.kt
+++ src/main/kotlin/be/athumi/WineShop.kt
@@ -3,54 +3,6 @@ package be.athumi
 class WineShop(var wines: List<Wine>) {
     fun next() {
         // Wine Shop logic
-        wines.forEach { wine ->
-            if (!isConservato(wine) && !isEventItem(wine)) {
-                if (wine.price > 0 && !isLegendaryItem(wine)) {
-                    wine.price -= 1
-                }
-            } else {
-                if (wine.price < 100) {
-                    wine.price += 1
-                    if (isEventItem(wine)) {
-                        if (wine.expiresInYears < 8 && wine.price < 100) {
-                            wine.price = wine.price + 1
-                        }
-
-                        if (wine.expiresInYears < 3 && wine.price < 100) {
-                            wine.price = wine.price + 2
-                        }
-                    }
-                }
-            }
-
-            if (!isLegendaryItem(wine)) {
-                wine.expiresInYears -= 1
-            }
-
-            if (wine.expiresInYears < 0) {
-                if (!isConservato(wine)) {
-                    if (!isEventItem(wine) && wine.price > 0 && !isLegendaryItem(wine)) {
-                        wine.price -= 1
-                    } else if (isEventItem(wine)) {
-                        wine.price = 0
-                    }
-                } else if (wine.price < 100) {
-                    wine.price += 1
-                }
-            }
-
-            if (wine.price < 0) {
-                wine.price = 0
-            }
-        }
+        wines.forEach { wine -> wine.update() }
     }
-
-    fun isConservato(wine: Wine): Boolean =
-            wine.name == "Bourdeaux Conservato" || wine.name == "Bourgogne Conservato"
-
-    fun isEventItem(wine: Wine): Boolean =
-            wine.name.startsWith("Event")
-
-    fun isLegendaryItem(wine: Wine): Boolean =
-            wine.name == "Wine brewed by Alexander the Great"
 }
